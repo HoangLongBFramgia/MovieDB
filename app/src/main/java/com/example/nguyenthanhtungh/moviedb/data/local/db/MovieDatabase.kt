a4d7fe640ffff139a152c4c173506c564e11f7a5
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2018-10-07 20:34:32 +0700

    init_gradle

--- app/src/main/java/com/example/nguyenthanhtungh/moviedb/data/local/db/MovieDatabase.kt
+++ app/src/main/java/com/example/nguyenthanhtungh/moviedb/data/local/db/MovieDatabase.kt
@@ -1,7 +1,7 @@
 package com.example.nguyenthanhtungh.moviedb.data.local.db
 
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.RoomDatabase
+import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.nguyenthanhtungh.moviedb.data.local.dao.MovieDao
 import com.example.nguyenthanhtungh.moviedb.data.model.Movie
 
--- app/src/main/java/com/example/nguyenthanhtungh/moviedb/ui/MainActivity.kt
+++ app/src/main/java/com/example/nguyenthanhtungh/moviedb/ui/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.example.nguyenthanhtungh.moviedb.ui.main
+package com.example.nguyenthanhtungh.moviedb.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.nguyenthanhtungh.moviedb.R
 
 class MainActivity : AppCompatActivity() {
