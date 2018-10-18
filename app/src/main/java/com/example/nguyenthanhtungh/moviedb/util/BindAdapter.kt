b0385b19598e43aceffcd56ca527311f193462e8
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2018-10-18 17:17:12 +0700

    Glide cached

--- app/src/main/java/com/example/nguyenthanhtungh/moviedb/util/BindAdapter.kt
+++ app/src/main/java/com/example/nguyenthanhtungh/moviedb/util/BindAdapter.kt
@@ -3,6 +3,7 @@ package com.example.nguyenthanhtungh.moviedb.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.nguyenthanhtungh.moviedb.R
 
@@ -12,7 +13,8 @@ object BindAdapter {
     fun loadImage(imageView: ImageView, image: String?) {
         Glide.with(imageView.context)
                 .load(BASE_IMAGE_URL + image)
-                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher_background))
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher_background)
+                        .diskCacheStrategy(DiskCacheStrategy.DATA))
                 .into(imageView)
     }
 }
