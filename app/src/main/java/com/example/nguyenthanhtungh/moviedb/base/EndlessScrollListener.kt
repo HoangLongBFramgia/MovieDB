70bac7af88a44eb61ad5e9cbf21f9fb2e0a07e4c
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2018-10-19 14:02:36 +0700

    fix_load_more

--- app/src/main/java/com/example/nguyenthanhtungh/moviedb/base/EndlessScrollListener.kt
+++ app/src/main/java/com/example/nguyenthanhtungh/moviedb/base/EndlessScrollListener.kt
@@ -1,7 +1,5 @@
 package com.example.nguyenthanhtungh.moviedb.base
 
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class EndlessScrollListener(val onLoadMore: () -> Unit) : RecyclerView.OnScrollListener() {
@@ -9,27 +7,7 @@ class EndlessScrollListener(val onLoadMore: () -> Unit) : RecyclerView.OnScrollL
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-
-        var pastItem = 0
-        var visibleItem = 0
-        var totalItem = 0
-        when (recyclerView?.layoutManager) {
-            is LinearLayoutManager -> {
-                (recyclerView.layoutManager as LinearLayoutManager).apply {
-                    pastItem = findFirstVisibleItemPosition()
-                    visibleItem=childCount
-                    totalItem = itemCount
-                }
-            }
-            is GridLayoutManager -> {
-                (recyclerView.layoutManager as GridLayoutManager).apply {
-                    pastItem = findFirstVisibleItemPosition()
-                    visibleItem = childCount
-                    totalItem = itemCount
-                }
-            }
-        }
-        if (!isLoading && pastItem + visibleItem >= totalItem) {
+        if (!isLoading && !recyclerView.canScrollVertically(1)) {
             isLoading = true
             onLoadMore.invoke()
         }
